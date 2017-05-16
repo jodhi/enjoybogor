9afb5126f9b6b9dbf3ff2c1f721650e686cd69b3
jodhi <jodhilesmana@gmail.com>
2017-05-17 05:44:51 +0700

    first try to add map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { UserData } from '../../providers/user-data';
 
+declare var google;
 // todo: refresh page get data;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
   constructor(public navCtrl: NavController, public http: Http, public toastCtrl: ToastController, public userData: UserData) {
 
@@ -17,9 +21,23 @@ export class HomePage {
 
   ionViewDidLoad() {
   	// this.getData();
+    this.loadMap();
   }
 
   ionViewWillEnter() {
 
   }
+  loadMap(){
+
+  let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+  let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+}
 }
