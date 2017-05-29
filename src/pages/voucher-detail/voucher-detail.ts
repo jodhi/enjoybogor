ae9cd0d71d50605dafaeffbbf774137260d69507
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-05-30 00:44:37 +0700

    implement voucher get

--- src/pages/voucher-detail/voucher-detail.ts
+++ src/pages/voucher-detail/voucher-detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams ,ToastController} from 'ionic-angular';
 import { Http } from '@angular/http';
 import { UserData } from '../../providers/user-data';
 
@@ -18,12 +18,23 @@ export class VoucherDetailPage {
   public id: any;
   public posts: any;
   public comments: any;
+  public taken: any;
+  public user_id:string;
   public sharePic;
+  public points: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http,public userData: UserData) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http,public userData: UserData,public toastCtrl: ToastController) {
     this.id = navParams.data;
     console.log("id nya : ",this.id);
     this.getData();
+    this.userData.getID().then((user_id) => {
+      this.user_id = user_id;
+    });
+    this.userData.getPoints().then((points) => {
+      this.points = points;
+    });
+    this.taken = 0;
   }
 
 
@@ -57,6 +68,26 @@ export class VoucherDetailPage {
 
 takeVoucher(){
   console.log("take voucher");
+  let data_send = JSON.stringify({ voucher_id: this.id,user_id: this.user_id , points: this.points });
+  this.http.post('http://localhost/enjoybogor-backend/api/take_voucher.php', data_send).subscribe(res => {
+    let response = res.json();
+    console.log(response);
+    if (response['status']=='success'){
+      console.log("success update");
+      this.taken=1;
+      this.navCtrl.pop();
+    }
+    this.showToast(response['message']);
+  });
 }
 
+showToast(val){
+    let toast = this.toastCtrl.create({
+      message: val,
+      duration: 3500,
+      position: 'top'
+    });
+    toast.present();
+
+}
 }
