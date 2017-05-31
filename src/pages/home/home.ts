bf21762c87adeb8a6f7d790e8ebc39aa2dab4fe5
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-05-31 16:13:25 +0700

    for presentation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { UserData } from '../../providers/user-data';
+import { ArtikelBacaPage } from '../artikel-baca/artikel-baca';
 
 declare var google;
 // todo: refresh page get data;
@@ -10,11 +11,11 @@ declare var google;
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-
+  public data;
+  public httpErr = false;
 
   constructor(public navCtrl: NavController, public http: Http, public toastCtrl: ToastController, public userData: UserData) {
-
+    this.getData();
   }
 
 
@@ -23,8 +24,42 @@ export class HomePage {
   }
 
   ionViewWillEnter() {
+this.getData();
+  }
+
+  getData() {
+    this.http.get('http://localhost/enjoybogor-backend/api/top3.php').subscribe(res => {
+      this.data = res.json();
+      console.log('dapet data restaurant');
+      this.httpErr = false;
+    }, err => {this.showAlert(err.status)});
+
 
   }
 
+  baca(id_restaurant){
+    this.navCtrl.push(ArtikelBacaPage, id_restaurant);
+  }
 
+  showAlert(status){
+   if(status == 0){
+     let toast = this.toastCtrl.create({
+       message: 'No connection, Please check your internet connection',
+       position: 'bottom',
+       showCloseButton: true,
+       closeButtonText: 'X'
+     });
+     toast.present();
+   }else{
+     let toast = this.toastCtrl.create({
+       message: 'Cannot connect to server, please reload this page',
+       position: 'bottom',
+       showCloseButton: true,
+       closeButtonText: 'X'
+     });
+     toast.present();
+   }
+
+   this.httpErr = true;
+ }
 }
