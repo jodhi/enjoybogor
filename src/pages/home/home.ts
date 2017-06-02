02e8752b23cae1022b9d4dc0efe44a0f807ceaf5
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-06-02 22:38:37 +0700

    bug fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { UserData } from '../../providers/user-data';
